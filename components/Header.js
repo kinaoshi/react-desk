46f3a9b432a80dcdbe6da3e2ab44c911a7effbd1
kinaoshi <ku.928.maid@gmail.com>
2021-08-21 10:48:55 +0900

    ✨ ヘッダーの追加

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import Link from "next/Link"
+import classes from "./Headre.module.css"
+
+export function Header() {
+
+    return (
+        <header className={classes.header}>
+            <Link href="/">
+            <a className={classes.anchor}>Index</a>
+            </Link>
+            <Link  href="/about">
+            <a className={classes.anchor}>About</a>
+            </Link>
+            
+
+        </header>
+    )
+}
