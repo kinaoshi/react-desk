6027d04b661656d25f9a51d37e5ea49988e7df6f
kinaoshi <ku.928.maid@gmail.com>
2021-08-23 20:58:08 +0900

    ✨ useState で文字列を真偽値について解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,38 +5,44 @@ import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import classes from "../styles/Home.module.css";
 
-
-
 export default function Home() {
-   const [count, setCount] = useState(1);
-	
-	const handleClick = useCallback (()=>  {
-	  if (count < 10){
-		  setCount ((count) => count + 1);
-	  }
-		
-	},[count]);
-    useEffect(() => {
-		
-		document.body.style.backgroundColor="lightblue"
-		return ()=> {
-			
-			document.body.style.backgroundColor=""
+	const [count, setCount] = useState(1);
+	const [text, setText] = useState("");
+
+	const handleClick = useCallback(() => {
+		if (count < 10) {
+			setCount((count) => count + 1);
 		}
-	},[count]);
+	}, [count]);
+	useEffect(() => {
+		document.body.style.backgroundColor = "lightblue";
+		return () => {
+			document.body.style.backgroundColor = "";
+		};
+	}, [count]);
+	const handleChange = useCallback((e) => {
+		if (e.target.value.length >= 5) {
+			alert("5文字以内にして下さい");
+			return;
+		}
+		setText(e.target.value.trim());
+	}, []);
+	const [isShow, setIsShow] = useState(true);
+	const handleDisplay = () => {
+		setIsShow((isShow) => !isShow);
+	};
 	return (
 		<div className={classes.container}>
 			<Head>
 				<title>Index Page</title>
 			</Head>
 			<Header />
-			<h1>{count}</h1>
-			<button
-				href="./about"
-				onClick={handleClick}
-			>
-				カウンター
-			</button><span>#13から</span>
+			{isShow ? <h1>{count}</h1> : null}
+			<button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+			<button href="./about" onClick={handleClick}>
+				カウンター#15
+			</button>
+			<input type="text" value={text} onChange={handleChange} />
 			　　　　　　
 			<Main page="index" />
 			<Footer />
