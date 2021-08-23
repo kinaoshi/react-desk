239989cda9a8d3e322ce68d318b7958f691a6e6f
kinaoshi <ku.928.maid@gmail.com>
2021-08-23 12:55:03 +0900

    ⚗️ 不要な記述でいつか消しますが試しに書いてみました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,35 @@
 import Head from "next/head";
+import { useCallback } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import classes from "../styles/Home.module.css";
 
+
+
 export default function Home() {
+  
+	const handleClick=useCallback((e)=>  {
+		const foo = 1;
+		console.log(e.target.href);
+		e.preventDefault();
+		alert(foo);
+	},[]);
+    
 	return (
 		<div className={classes.container}>
 			<Head>
 				<title>Index Page</title>
 			</Head>
 			<Header />
-　　　　　　
+			<a
+				href="./about"
+				onClick={handleClick}
+			>
+				ボタン
+			</a>
+			　　　　　　
 			<Main page="index" />
-
 			<Footer />
 		</div>
 	);
