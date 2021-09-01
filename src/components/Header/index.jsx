ea211f0a8c25cbf37d6dd22076a5d4850ccbdb5c
kinaoshi <ku.928.maid@gmail.com>
2021-09-01 19:44:09 +0900

    users,comentsの追加

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,6 +6,8 @@ export const Header = () => {
 	const NAV_ITEMS = [
 		{ href: "/", label: "Index" },
 		{ href: "/posts", label: "Posts" },
+		{ href: "/users", label: "Users" },
+		{ href: "/comments", label: "Comments" },
 	];
 	return (
 		<header className={classes.header}>
@@ -16,7 +18,6 @@ export const Header = () => {
 					</Link>
 				);
 			})}
-	
 		</header>
 	);
 };
