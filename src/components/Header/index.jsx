f61096a7a3976b07b3cd8ac2f11578016c0ddbed
kinaoshi <ku.928.maid@gmail.com>
2021-09-08 09:09:54 +0900

    💄 TailWind Cssを使って既存のスタイルの書き換え

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,20 @@
 import Link from "next/link";
-import classes from "src/components/Header/Header.module.css";
+
 
 
 export const Header = () => {
 	const NAV_ITEMS = [
-		{ href: "/", label: "Index#28" },
+		{ href: "/", label: "Index#29" },
 		{ href: "/posts", label: "Posts" },
 		{ href: "/users", label: "Users" },
 		{ href: "/comments", label: "Comments" },
 	];
 	return (
-		<header className={classes.header}>
+		<header className="flex justify-center items-center border-b min-w-full h-24 mb-4">
 			{NAV_ITEMS.map((item) => {
 				return (
 					<Link key={item.href} href={item.href}>
-						<a className={classes.anchor}>{item.label}</a>
+						<a className="inline-block py-2 px-6 text-xl  hover:text-blue-500 focus:text-pink-600 active:text-green-600">{item.label}</a>
 					</Link>
 				);
 			})}
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,5 +1,14 @@
-import classes from "src/components/Layout/Layout.module.css";
+
 
 export const Layout = (props) => {
-	return <div className={classes.container}>{props.children}</div>;
+	return <div className="flex flex-col items-center mx-auto px-2 min-h-screen max-w-2xl">{props.children}</div>;
 };
+
+// min-height: 100vh;
+//   padding: 0 0.5rem;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   height: 100vh;
+//   max-width: 600px;
+//   margin: 0 auto;
